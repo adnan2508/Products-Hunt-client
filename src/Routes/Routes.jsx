2a81d83885069e3daed8346a1adb385d80ad2ccd
adnan2508 <adnandiu2800@gmail.com>
2024-08-15 22:22:07 +0600

    register and login added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main/>,
+    children: [
+      {
+        path: "/register",
+        element: <Register/>,
+      },
+      {
+        path: "/login",
+        element: <Login/>,
+      },
+    ]
   },
 ]);
 
