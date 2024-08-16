0e3cbdc1ab800a73a2862b2b878f038cb008f384
adnan2508 <adnandiu2800@gmail.com>
2024-08-16 20:01:03 +0600

    product added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,17 +11,14 @@ const ProductCard = ({product}) => {
   />
 
   <div className="bg-white p-4 sm:p-6">
-    <time datetime="2022-10-10" className="block text-xs text-gray-500"> 10th Oct 2022 </time>
+    <time datetime="2022-10-10" className="block text-xs text-gray-500"> {product.category} </time>
 
     <a href="#">
       <h3 className="mt-0.5 text-lg text-gray-900">{product.title}</h3>
     </a>
 
     <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-      pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
-      mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
-      dignissimos. Molestias explicabo corporis voluptatem?
+      {product.description}
     </p>
   </div>
 </article>
