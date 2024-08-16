1b73290a9aad9e39bc9c3ae7c3518b664ff8f544
adnan2508 <adnandiu2800@gmail.com>
2024-08-16 19:51:51 +0600

    title added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
     <div>
       <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
   <img
     alt=""
-    src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+    src={product.image}
     className="h-56 w-full object-cover"
   />
 
@@ -14,7 +14,7 @@ const ProductCard = () => {
     <time datetime="2022-10-10" className="block text-xs text-gray-500"> 10th Oct 2022 </time>
 
     <a href="#">
-      <h3 className="mt-0.5 text-lg text-gray-900">How to position your furniture for positivity</h3>
+      <h3 className="mt-0.5 text-lg text-gray-900">{product.title}</h3>
     </a>
 
     <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
